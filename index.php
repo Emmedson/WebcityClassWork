e91520454d722181b441ed941311bb40e638ac61
Emaedson324 <livinusdogz@gmail.com>
2023-09-09 13:49:45 +0100

    Class work for student refference

--- index.php
+++ index.php
@@ -3,7 +3,21 @@
 $host = "localhost";
 $user = "root";
 $passwor = "";
+$dbname = "phpdb";
 
-echo("Hello World!");
+$connect = mysqli_connect($host, $user, $passwor, $phpdb);
+if(!$connect){
+    die ("Connection failed:" . mysqli_connect_error());
+}
+else echo("Connected Successfully");
+
+// CREATE THE DATABASE //
+
+$sql = "CREATE DATABASE phpdb";
+
+if ($connect->query($sql) === TRUE) {
+    echo ("Database Created Successfully");
+} 
+else die ("Connection failed:" . mysqli_connect_error());
 
 ?>
